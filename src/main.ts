52d08e1bd2a477f6d57153709988f1af5900894a
Bryant Perez Garcia <bryantperezgarcia005@gmail.com>
2024-05-23 14:06:54 -0400

    [FEAT] swagger integration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
-import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Fitness Api')
+    .setDescription('An Api FOR HEALTY')
+    .setVersion('1.0')
+    .build();
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(configService.get<number>('PORT') || 0);
 }
-bootstrap();
+bootstrap().then(() => {
+  Logger.log('Application is up and running 🚀');
+});
