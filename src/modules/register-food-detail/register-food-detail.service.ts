16b1ea77052ed05ebcbfcb17554249fcc9965e85
Bryant Perez Garcia <bryantperezgarcia005@gmail.com>
2024-06-04 15:30:26 -0400

    just cleaning code

--- src/modules/register-food-detail/register-food-detail.service.ts
+++ src/modules/register-food-detail/register-food-detail.service.ts
@@ -3,15 +3,12 @@ import { CreateRegisterFoodDetailDto } from './dto/create-register-food-detail.d
 import { RegisterFoodDetail } from './entities/register-food-detail.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { RegisterFood } from '../register-food/entities/register-food.entity';
 
 @Injectable()
 export class RegisterFoodDetailService {
   constructor(
     @InjectRepository(RegisterFoodDetail)
     private readonly registerFoodDetailRepository: Repository<RegisterFoodDetail>,
-    @InjectRepository(RegisterFood)
-    private readonly registerFoodRepository: Repository<RegisterFood>,
   ) {}
 
   async create(createRegisterFoodDetailDto: CreateRegisterFoodDetailDto) {
